4331dcd44750778ea1c18f60f59c8eef05b02dcb
takayuki74 <76391096+takayuki74@users.noreply.github.com>
2021-02-14 10:42:11 +0900

    initial commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <numeric>
+using namespace std;
+using ll = long long;
+//ループ
+#define REP(i, n) for(int i = 0; i < n; ++i)
+#define REPR(i, n) for(int i = n - 1; i >= 0; --i)
+#define FOR(i, m, n) for(int i = m; i < n; ++i)
+//コンテナ
+#define all(x) (x).begin(),(x).end()
+//関数
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
+
+//-----global variable------------
+
+
+//-----function definition--------
+
+
+//-----main-----------------------
+int main() {
+    //----code here------------
+
+
+
+    //----return---------------
+    return 0;
+}
